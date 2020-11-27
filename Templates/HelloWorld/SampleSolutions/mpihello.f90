605cd0187cff1fba3e9edffcf5b078fd8be85f9f
jhein32 <joachim.hein@math.lu.se>
2020-11-27 12:16:00 +0100

    Update mpihello.f90
    formatting sugar

--- Templates/HelloWorld/SampleSolutions/mpihello.f90
+++ Templates/HelloWorld/SampleSolutions/mpihello.f90
@@ -1,4 +1,4 @@
-Program Mpihello
+program Mpihello
 
   use mpi
 
@@ -7,13 +7,12 @@ Program Mpihello
   Integer :: world_size, world_rank
   Integer :: merror
 
-
   Call MPI_Init(merror)
-
 
   Call MPI_Comm_size(MPI_COMM_WORLD, world_size, merror)
   Call MPI_Comm_rank(MPI_Comm_WORLD, world_rank, merror)
 
   print *,"I am rank ", world_rank," out of", world_size
 
-end Program Mpihello
+end program Mpihello
+
