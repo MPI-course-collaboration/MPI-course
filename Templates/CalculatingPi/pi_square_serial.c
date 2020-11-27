d106ed348f4c84185dad0faa28da63924b5d9e47
Joachim Hein <joachim.hein@math.lu.se>
2020-11-27 14:18:47 +0100

    template in C for the point-to-point exercise

---
+++ Templates/CalculatingPi/pi_square_serial.c
@@ -0,0 +1,17 @@
+int main()
+{
+
+  const int finval = 1000000000;
+
+  double pi_square = 0.0;
+
+  for ( int i=1 ; i <= finval; i++)
+    {  
+      double factor = (double) i;
+      pi_square += 1.0/( factor * factor );
+    }
+
+  printf ( "Pi^2 = %.10f\n", pi_square*6.0);
+
+  return 0;
+}
