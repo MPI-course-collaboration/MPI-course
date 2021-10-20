f9ef91e9ab59a6a09f40996dc4973ab1bb153a4e
Joachim Hein <joachim.hein@math.lu.se>
2021-10-20 17:53:59 +0200

    Update run_debug_hpc2n.sh

--- Templates/Day_3/Debugging/Demo/HPC2N/run_debug_hpc2n.sh
+++ Templates/Day_3/Debugging/Demo/HPC2N/run_debug_hpc2n.sh
@@ -2,8 +2,8 @@
 #SBATCH -t 00:20:00
 #SBATCH -n 4
 #SBATCH -N 1
-#SBATCH -A SNIC2020-9-175
-#SBATCH --reservation=SNIC2020-9-175-day3
+#SBATCH -A SNIC2021-22-733
+#SBATCH --reservation=snic2021-22-733-day3
 
 #SBATCH -o result_mpihello_%j.out
 #SBATCH -e result_mpihello_%j.out
@@ -11,8 +11,8 @@
 cat $0
 
 ml purge
-ml foss/2019b
-ml Forge/20.2-Ubuntu-16.04-x86_64
+ml foss/2021a
+ml Forge/21.0.2-linux-x86_64
 
-ddt --connect mpirun mpihello
+ddt --connect srun mpihello
 
