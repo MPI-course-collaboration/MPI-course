9dd28faf3c93681d8439e636efff1aa058eafbeb
jhein32 <joachim.hein@math.lu.se>
2021-10-21 18:34:21 +0200

    Update run_debug_python_hpc2n.sh

--- Templates/Day_3/Debugging/Demo/HPC2N/run_debug_python_hpc2n.sh
+++ Templates/Day_3/Debugging/Demo/HPC2N/run_debug_python_hpc2n.sh
@@ -2,8 +2,8 @@
 #SBATCH -t 00:20:00
 #SBATCH -n 4
 #SBATCH -N 1
-#SBATCH -A SNIC2020-9-175
-#SBATCH --reservation=SNIC2020-9-175-day3
+#SBATCH -A SNIC2021-22-733
+#SBATCH --reservation=snic2021-22-733-day3
 
 #SBATCH -o result_mpihello_%j.out
 #SBATCH -e result_mpihello_%j.out
@@ -11,10 +11,9 @@
 cat $0
 
 ml purge
-ml foss/2019b
-ml Python/3.7.4
-ml SciPy-bundle/2019.10-Python-3.7.4
-ml Forge/20.2-Ubuntu-16.04-x86_64
+ml foss/2021a
+ml SciPy-bundle/2021.05 
+ml Forge/21.0.2-linux-x86_64
 
 ddt --connect mpirun python3 %allinea_python_debug%  hello_mpi.py
 
