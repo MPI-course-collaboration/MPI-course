e51dcbe4a0db220f11cd1dcc71259ed525adfea6
Xin Li <lixin.reco@gmail.com>
2021-10-22 11:31:48 +0200

    updated python example for 2d integration (day 2)

--- Templates/Day_2/Collectives/Integration2D/Python/integration2D_serial.py
+++ Templates/Day_2/Collectives/Integration2D/Python/integration2D_serial.py
@@ -1,32 +1,25 @@
-from time import process_time
+import time
 import math
 import sys
 
-
 n = int(sys.argv[1])
 
+starttime = time.time()
 
-#turn on the stop watch
-starttime = process_time()
-
-#calculate the interval size, same for X and Y
-h = math.pi / float(n)
+# interval size (same for X and Y)
+h = math.pi / n
 
 mysum = 0.0
 
-#distribute work in the X axis
 for i in range(n):
     x = h * (i + 0.5)
-    #do regular integration in the Y axis
     for j in range(n):
         y = h * (j + 0.5)
         mysum += math.sin(x + y)
 
 integral = h**2 * mysum
 
-#turn off the stop watch
-endtime = process_time()
+endtime = time.time()
 
-#print results on the root rank
-print("Integral value is %.18f, Error is %.18f" % (integral, abs(integral - 0.0)))
-print("Time for loop and reduce is %.16f seconds" % (endtime-starttime))
+print("Integral value is %e, Error is %e" % (integral, abs(integral - 0.0)))
+print("Time spent: %.2f sec" % (endtime-starttime))
--- Templates/Day_2/Collectives/Integration2D/SampleSolution/Python/run.sh
+++ Templates/Day_2/Collectives/Integration2D/SampleSolution/Python/run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH -t 00:05:00
 #SBATCH -n 2
-#SBATCH -A Project_ID
-#SBATCH --reservation=reserv
+#SBATCH -A SNIC2021-22-733
+#SBATCH --reservation=snic2021-22-733-day2
 #SBATCH -o result_%j.out
 #SBATCH -e result_%j.out
 
 cat $0
 
 ml purge
-ml foss/2019b
-ml Python/3.7.4 SciPy-bundle/2019.10-Python-3.7.4
+ml foss/2021a
+ml SciPy-bundle/2021.05
 
 mpirun python3 $* 
