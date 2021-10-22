8312e2cd2d914551629152e8c0a5850b795e484e
Xin Li <lixin.reco@gmail.com>
2021-10-22 11:10:43 +0200

    updated python example for calculating PI (day 1)

--- Templates/Day_1/CalculatingPi/SampleSolution/Python/pi_square_parallel.py
+++ Templates/Day_1/CalculatingPi/SampleSolution/Python/pi_square_parallel.py
@@ -1,4 +1,5 @@
 from mpi4py import MPI
+import time
 
 comm = MPI.COMM_WORLD
 
@@ -8,6 +9,8 @@ numprocs = comm.Get_size()
 # we use a smaller finval than in the C code
 # since loop in python is not as efficient
 finval = 10000000
+
+t0 = time.time()
 
 pi_square = 0.0
 
@@ -20,3 +23,4 @@ else:
     for source in range(1, numprocs):
         pi_square += comm.recv(source=source, tag=source)
     print("Pi^2 = {:.10f}".format(pi_square*6.0))
+    print("Time spent: {:.2f} sec".format(time.time() - t0))
--- Templates/Day_1/CalculatingPi/SampleSolution/Python/readme.txt
+++
@@ -1,7 +0,0 @@
-Here are two different sample solutions.  
-
-pi_square_parallel.py distributes the array indices in a round robin 
-     fashion onto the tasks. 
-
-pi_square_parallel_blocked.py creates continuous block of array indices, 
-     which get distributed onto the processors.
