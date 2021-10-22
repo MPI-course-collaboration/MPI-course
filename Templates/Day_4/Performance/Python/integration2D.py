dec44106a16a190dde3ae04ca8d9577c2e24ca7b
Xin Li <lixin.reco@gmail.com>
2021-10-22 11:42:09 +0200

    updated python example for performance (day 4)

--- Templates/Day_4/Performance/Python/integration2D.py
+++ Templates/Day_4/Performance/Python/integration2D.py
@@ -7,42 +7,33 @@ comm = MPI.COMM_WORLD
 numprocs = comm.Get_size()
 myrank = comm.Get_rank()
 
-if myrank == 0:
-    n = int(sys.argv[1])
-else:
-    n = None
-
-#broadcast n
-n = comm.bcast(n, root=0)
+n = int(sys.argv[1])
 
 if n <= 0:
     comm.Abort(-1)
 
-#turn on the stop watch
 starttime = MPI.Wtime()
 
-#calculate the interval size, same for X and Y
+# interval size (same for X and Y)
 h = math.pi / float(n)
 
 mysum = 0.0
 
-#distribute work in the X axis
+# distribute work in the X axis
 for i in range(myrank, n, numprocs):
     x = h * (i + 0.5)
-    #do regular integration in the Y axis
+    # regular integration in the Y axis
     for j in range(n):
         y = h * (j + 0.5)
         mysum += math.sin(x + y)
 
 local_integral = h**2 * mysum
 
-#do the reduction
+# reduction
 integral = comm.reduce(local_integral, op=MPI.SUM, root=0)
 
-#turn off the stop watch
 endtime = MPI.Wtime()
 
-#print results on the root rank
 if myrank == 0:
-    print("Integral value is %.18f, Error is %.18f" % (integral, abs(integral - 0.0)))
-    print("Time for loop and reduce is %.16f seconds" % (endtime-starttime))
+    print("Integral value is %e, Error is %e" % (integral, abs(integral - 0.0)))
+    print("Time spent: %.2f sec" % (endtime-starttime))
--- Templates/Day_4/Performance/Python/run.sh
+++ Templates/Day_4/Performance/Python/run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #SBATCH -t 00:05:00
 #SBATCH -n 2
-#SBATCH -A SNIC2020-9-175
+#SBATCH -A SNIC2021-22-733
+#SBATCH --reservation=snic2021-22-733-day4
 #SBATCH -o result_%j.out
 #SBATCH -e result_%j.out
 
 cat $0
 
 ml purge
-ml foss/2019b
-ml Python/3.7.4 SciPy-bundle/2019.10-Python-3.7.4
+ml foss/2021a
+ml SciPy-bundle/2021.05
 
 mpirun python3 $* 
