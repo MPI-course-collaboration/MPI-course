6aefcce545e7852a04faeca0afd0dbf2784c66bf
Joachim Hein <joachim.hein@math.lu.se>
2022-10-12 18:29:53 +0200

    Update of submission scripts for debug demo for module recommendation and account

--- Templates/Day_3/Debugging/Demo/HPC2N/run_debug_hpc2n.sh
+++ Templates/Day_3/Debugging/Demo/HPC2N/run_debug_hpc2n.sh
@@ -2,8 +2,8 @@
 #SBATCH -t 00:20:00
 #SBATCH -n 4
 #SBATCH -N 1
-#SBATCH -A SNIC2021-22-733
-#SBATCH --reservation=snic2021-22-733-day3
+#SBATCH -A SNIC2022-22-890
+#SBATCH --reservation=mpi-course-day3
 
 #SBATCH -o result_mpihello_%j.out
 #SBATCH -e result_mpihello_%j.out
@@ -11,8 +11,8 @@
 cat $0
 
 ml purge
-ml foss/2021a
-ml Forge/21.0.2-linux-x86_64
+ml foss/2021b
+ml Forge/22.0.1-linux-x86_64
 
 ddt --connect mpirun ./mpihello
 
--- Templates/Day_3/Debugging/Demo/HPC2N/run_debug_python_hpc2n.sh
+++ Templates/Day_3/Debugging/Demo/HPC2N/run_debug_python_hpc2n.sh
@@ -2,8 +2,8 @@
 #SBATCH -t 00:20:00
 #SBATCH -n 4
 #SBATCH -N 1
-#SBATCH -A SNIC2021-22-733
-#SBATCH --reservation=snic2021-22-733-day3
+#SBATCH -A SNIC2022-22-890
+#SBATCH --reservation=mpi-course-day3
 
 #SBATCH -o result_mpihello_%j.out
 #SBATCH -e result_mpihello_%j.out
@@ -11,9 +11,10 @@
 cat $0
 
 ml purge
-ml foss/2020b
-ml SciPy-bundle/2020.11 
-ml Forge/21.0.2-linux-x86_64
+ml foss/2021b
+ml Python/3.9.6
+ml SciPy-bundle/2021.10 
+ml Forge/22.0.1-linux-x86_64
 
 ddt --connect mpirun python3 %allinea_python_debug%  hello_mpi.py
 
