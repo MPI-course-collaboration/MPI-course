de2ef8b901f3393385d55cd6011c14e4bfdc24d6
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2025-08-22 14:27:21 +0200

    Update C3SE run scripts
    There is currently (and no plans to add) DDT or other parallel MPI
    debugger on Vera. Also updated modules for run scripts in repo.

--- Templates/Day_1/RunningCode/C3SE/run_hello_c3se.sh
+++ Templates/Day_1/RunningCode/C3SE/run_hello_c3se.sh
@@ -2,9 +2,8 @@
 #SBATCH -t 00:05:00
 #SBATCH -n 4
 #SBATCH -A YOUR_PROJECT
-#SBATCH --reservation=snic_mpi
 
 ml purge
-ml foss/2019b
+ml foss
 
-mpirun mpihello
+srun mpihello
--- Templates/Day_1/RunningCode/C3SE/run_hello_python_c3se.sh
+++ Templates/Day_1/RunningCode/C3SE/run_hello_python_c3se.sh
@@ -2,12 +2,8 @@
 #SBATCH -t 00:05:00
 #SBATCH -n 4
 #SBATCH -A YOUR_PROJECT
-#SBATCH --reservation=snic_mpi
 
 ml purge
-ml foss/2019b
+ml mpi4py
 
-ml Python/3.7.4
-ml SciPy-bundle/2019.10-Python-3.7.4
-
-mpirun python3 hello_mpi.py
+srun python3 hello_mpi.py
