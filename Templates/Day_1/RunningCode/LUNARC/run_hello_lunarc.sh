cebe70284808c3ffdda7853e4cf960d3c0a057ce
Joachim Hein <joachim.hein@math.lu.se>
2025-09-08 17:55:30 +0200

    Updates of basic job scripts for the 2025 running of the course

--- Templates/Day_1/RunningCode/LUNARC/run_hello_lunarc.sh
+++ Templates/Day_1/RunningCode/LUNARC/run_hello_lunarc.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+
+## Basics
 #SBATCH -t 00:05:00
 #SBATCH -N 1                   # number of nodes
 #SBATCH --tasks-per-node=4     # number of processes per node
 #SBATCH -t 00:05:00            # job-time – here 5 min
 
-#SBATCH -A lu2024-7-94
-#SBATCH --reservation=MPI-course
-##SBATCH --reservation=MPI-course2
-##SBATCH --reservation=MPI-course3
-##SBATCH --reservation=MPI-course4
+## Accounting/Reservation
+#SBATCH -A lu2025-7-75 
+#SBATCH --reservation=MPI-course-1day
+##SBATCH --reservation=MPI-course-2day
+##SBATCH --reservation=MPI-course-3day
+##SBATCH --reservation=MPI-course-4day
 
-
+## Output/Error files
 #SBATCH -o result_mpihello_%j.out
 #SBATCH -e result_mpihello_%j.out
 
