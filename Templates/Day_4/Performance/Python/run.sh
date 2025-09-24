2728c17bd24cf826cb5e7a17ab7e0b8cad21825b
Pedro Ojeda-May <pedroojeda2011@gmail.com>
2025-09-24 07:40:35 +0200

    Day4: performance batch script tool chain

--- Templates/Day_4/Performance/Python/run.sh
+++ Templates/Day_4/Performance/Python/run.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
+#SBATCH -N 1                   # number of nodes
+#SBATCH --tasks-per-node=1     # number of processes per node
+#SBATCH -t 00:08:00            # job-time – here 5 min
 
-#SBATCH -A Project_ID
-#SBATCH --reservation=reserv
+#SBATCH -A lu2025-7-75
+##SBATCH --reservation=mpi-course-1day
+##SBATCH --reservation=mpi-course-2day
+##SBATCH --reservation=mpi-course-3day
+##SBATCH --reservation=mpi-course-4day
 
-#SBATCH -t 00:05:00
-#SBATCH -n 2
-
-#SBATCH -o result_%j.out
-#SBATCH -e result_%j.out
+#SBATCH -o result_mpihello_%j.out
+#SBATCH -e result_mpihello_%j.out
 
 cat $0
 
 ml purge
-ml foss/2021b
-ml Python/3.9.6
-ml SciPy-bundle/2021.10
+ml foss/2024a
+ml Python/3.12.3
+ml SciPy-bundle/2024.05
+ml mpi4py/4.0.1
 
-mpirun python3 $* 
+mpirun python3 $*
